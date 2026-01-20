312c87029b7a0a73ce77224a8f198b75c5382233
vincent mas <cvm.tpmr@gmail.com>
2026-01-20 21:54:27 +0100

    git commit -m "add child missions page"

---
+++ app/child/missions/page.tsx
@@ -0,0 +1,11 @@
+export default function ChildMissionsPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Page Missions enfant</h1>
+      <p>Si tu vois ce texte, la route fonctionne 🎉</p>
+    </div>
+  );
+}
+
+
+
