8ea338bc014496781cadee96e92143672453a77d
vincent mas <cvm.tpmr@gmail.com>
2026-01-20 20:56:55 +0100

    restore visible solar system

--- components/SolarSystem.tsx
+++ components/SolarSystem.tsx
@@ -1,18 +1,23 @@
+import styles from "./solar.module.css";
+
 export default function SolarSystem() {
   return (
-    <div
-      style={{
-        position: "fixed",
-        inset: 0,
-        background: "black",
-        color: "lime",
-        fontSize: "32px",
-        zIndex: 0,
-        pointerEvents: "none",
-      }}
-    >
-      🌞 SOLAR SYSTEM RENDU 🌞
+    <div className={styles.space}>
+      <div className={styles.sun}></div>
+
+      <div className={`${styles.orbit} ${styles.orbit1}`}>
+        <div className={`${styles.planet} ${styles.planet1}`} />
+      </div>
+
+      <div className={`${styles.orbit} ${styles.orbit2}`}>
+        <div className={`${styles.planet} ${styles.planet2}`} />
+      </div>
+
+      <div className={`${styles.orbit} ${styles.orbit3}`}>
+        <div className={`${styles.planet} ${styles.planet3}`} />
+      </div>
     </div>
   );
 }
+
 
